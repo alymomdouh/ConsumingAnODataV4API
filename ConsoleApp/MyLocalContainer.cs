5e1a25bd7cca3c4300ac6fd8b6c4ad9155af58be
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-24 11:03:57 +0200

    add operation for person

--- ConsoleApp/MyLocalContainer.cs
+++ ConsoleApp/MyLocalContainer.cs
@@ -11,6 +11,21 @@ namespace ConsoleApp
         public MyLocalContainer(Uri serviceRoot) : base(serviceRoot)
         {
             People = base.CreateQuery<Person>("People");
-        } 
+        }
+
+
+        // operation for person 
+        public void AddPerson(Person person)
+        {
+            base.AddObject("People", person);
+        }
+        public void UpdatePerson(Person person)
+        {
+            base.UpdateObject("People", person);
+        }
+        public void DeletePerson(Person person)
+        {
+            base.DeleteObject("People", person);
+        }
     }
 }
