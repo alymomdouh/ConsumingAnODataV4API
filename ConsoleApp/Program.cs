145737562da7bd896c4128a9460cd0efffc566a2
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-24 13:41:42 +0200

    inject auto generate dbcontext and make select all and eager loading of collection of NavigationProperty

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleApp.Models;
+﻿using AirVinyl;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,26 @@ namespace ConsoleApp
         {
 
             Console.WriteLine("Hello World!");
+
+            // call auto generated debcontext container 
+
+            var autoContainer = new AirVinylContainer(new Uri(BaseUrl));
+            var people = autoContainer.People.Execute();
+            foreach (var person in people.ToList())
+            {
+                Console.WriteLine($"{person.PersonId} {person.FirstName} {person.LastName}");
+                // view NavigationProperty Name="VinylRecords" Type="Collection"
+
+                //autoContainer.LoadProperty(person, "VinylRecords"); 
+                //foreach (var vinylRecord in person.VinylRecords)
+                //{
+                //    Console.WriteLine($"---- {vinylRecord.VinylRecordId} {vinylRecord.Title}");
+                //}
+            }
+        }
+
+        public async static Task NoCodeGeneration()
+        {
             var myContainer = new MyLocalContainer(new Uri(BaseUrl));
 
             // to load people from source to conatiner
@@ -24,7 +44,7 @@ namespace ConsoleApp
             }
 
             // to insert into people 
-            myContainer.AddPerson(new Person()
+            myContainer.AddPerson(new Models.Person()
             {
                 FirstName = "John the First",
                 LastName = "Smith",
@@ -44,14 +64,13 @@ namespace ConsoleApp
             firstPerson.FirstName = "tes";
             myContainer.UpdatePerson(firstPerson);
             myContainer.SaveChanges();
-
         }
         public static void LogTrackedPerson(MyLocalContainer container)
         {
             //view the status of all items 
             foreach (var entityDescriptor in container.EntityTracker.Entities)
             {
-                if (entityDescriptor.Entity is Person castedPerson)
+                if (entityDescriptor.Entity is Models.Person castedPerson)
                 {
                     Console.WriteLine($"{entityDescriptor.State} - {castedPerson.PersonId} " + $"{castedPerson.FirstName} {castedPerson.LastName}");
                 }
