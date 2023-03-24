0101179a4537f0ed2f4949c44747779eb647960a
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-24 11:04:44 +0200

    get list or all persons and depend on condition

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,12 +6,21 @@ namespace ConsoleApp
 {
     class Program
     {
+        private const string BaseUrl = "https://localhost:5001/odata";// link for api or backend 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var myContainer = new MyLocalContainer(new Uri("https://localhost:5001/odata"));
 
-            
+            Console.WriteLine("Hello World!");
+            var myContainer = new MyLocalContainer(new Uri(BaseUrl));
+
+            // to load people from source to conatiner
+            var people = myContainer.People.Execute();
+            var peopleCondition = myContainer.People.Where(a => a.PersonId == 1);
+            var peopleAsync = await myContainer.People.ExecuteAsync();
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.PersonId} {person.FirstName} {person.LastName}");
+            }
 
         }
     }
