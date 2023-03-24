c8bb2ffd103800a489dcf10f983158203e501698
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-24 11:24:31 +0200

    insert persons and make function to log status for persons

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp.Models;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,6 +23,35 @@ namespace ConsoleApp
                 Console.WriteLine($"{person.PersonId} {person.FirstName} {person.LastName}");
             }
 
+            // to insert into people 
+            myContainer.AddPerson(new Person()
+            {
+                FirstName = "John the First",
+                LastName = "Smith",
+                AmountOfCashToSpend = 400,
+                DateOfBirth = new DateTimeOffset(new DateTime(1980, 5, 10)),
+                Email = "someaddress@someserver.com",
+                NumberOfRecordsOnWishList = 10,
+            });
+            // note not pass the id it is auto generation 
+            // view status before save 
+            LogTrackedPerson(myContainer);
+            await myContainer.SaveChangesAsync();
+            // view status after save 
+            LogTrackedPerson(myContainer);
+
+
+        }
+        public static void LogTrackedPerson(MyLocalContainer container)
+        {
+            //view the status of all items 
+            foreach (var entityDescriptor in container.EntityTracker.Entities)
+            {
+                if (entityDescriptor.Entity is Person castedPerson)
+                {
+                    Console.WriteLine($"{entityDescriptor.State} - {castedPerson.PersonId} " + $"{castedPerson.FirstName} {castedPerson.LastName}");
+                }
+            }
         }
     }
 }
