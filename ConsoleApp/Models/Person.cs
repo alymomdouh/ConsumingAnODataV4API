d8e4e8b03c648dbdb6469d8761c5da2d3ebf984b
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-24 10:53:21 +0200

    add domain and dbcontext

---
+++ ConsoleApp/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OData.Client;
+using System;
+
+namespace ConsoleApp.Models
+{
+    [Key("PersonId")]
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public int NumberOfRecordsOnWishList { get; set; }
+        public decimal AmountOfCashToSpend { get; set; }
+    }
+
+}
---
+++ ConsoleApp/MyLocalContainer.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp.Models;
+using Microsoft.OData.Client;
+using System;
+
+namespace ConsoleApp
+{
+    public class MyLocalContainer : DataServiceContext
+    {
+        public DataServiceQuery<Person> People { get; }
+
+        public MyLocalContainer(Uri serviceRoot) : base(serviceRoot)
+        {
+            People = base.CreateQuery<Person>("People");
+        } 
+    }
+}
